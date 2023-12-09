63f152f13a49ed33413587ca9957e7fe3a33b57e
Isak Bego <isakbego@gmail.com>
2023-12-09 19:20:07 +0200

    Form closes afer submission

--- src/project.js
+++ src/project.js
@@ -12,13 +12,13 @@ export function Project(title){
     }
 
     this.awaitSubmission = () => {
-        let submitButton = document.querySelector(".submit"); 
+        let submitButton = document.querySelector("#submit"); 
         submitButton.addEventListener("click", () => {
-            const title = document.querySelector(".title").value; 
-            const description = document.querySelector(".desc").value; 
-            const dateInfo = parseDate(document.querySelector(".datepicker").value); 
+            const title = document.querySelector("#title").value; 
+            const description = document.querySelector("#desc").value; 
+            const dateInfo = parseDate(document.querySelector("#datepicker").value); 
             const date = new Date(dateInfo[0], dateInfo[1], dateInfo[2]); 
-            const priority = document.querySelector(".priority").value; 
+            const priority = document.querySelector("#priority").value; 
 
             this.addToDo(todo(title, description, date, priority));  
             console.log(this.toDoList);
@@ -27,20 +27,20 @@ export function Project(title){
     }
 
     this.createForm = () => {
-        createDOM("input", [["class", "title"], ["type", "text"]], document.body);
-        createDOM("input", [["class", "desc"], ["type", "text"]], document.body); 
-        createDOM("input", [["class", "datepicker"], ["type", "date"]], document.body); 
-        createDOM("input", [["class", "priority"], ["type", "text"]], document.body); 
-        createDOM("button", [["class", "submit"]], document.body, "Submit");
+        createDOM("input", [["id", "title"], ["type", "text"]], document.body);
+        createDOM("input", [["id", "desc"], ["type", "text"]], document.body); 
+        createDOM("input", [["id", "datepicker"], ["type", "date"]], document.body); 
+        createDOM("input", [["id", "priority"], ["type", "text"]], document.body); 
+        createDOM("button", [["id", "submit"]], document.body, "Submit");
         this.awaitSubmission(); 
     }
 
     this.removeForm = () => {
-        removeDOM(); 
-        removeDOM(); 
-        removeDOM(); 
-        removeDOM(); 
-        removeDOM(); 
+        removeDOM(["id", "title"]); 
+        removeDOM(["id", "desc"]); 
+        removeDOM(["id", "datepicker"]); 
+        removeDOM(["id", "priority"]); 
+        removeDOM(["id", "submit"]); 
     }
 
     
